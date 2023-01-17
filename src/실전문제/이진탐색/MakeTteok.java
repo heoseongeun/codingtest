ed6c622b6309869a02d1f6c4e615d27a14796790
heoseongeun <rhdwnsla1223@naver.com>
2023-01-17 21:11:32 +0900

    떡볶이 떡 만들기 이진탐색으로 풀기

--- src/실전문제/이진탐색/MakeTteok.java
+++ src/실전문제/이진탐색/MakeTteok.java
@@ -20,6 +20,8 @@ public class MakeTteok {
         }
         long finishTime = System.currentTimeMillis();
         System.out.println(finishTime - startTime);
+
+        System.out.println(binary(arr, M));
     }
 
     public static int tteokLength(int[] arr, int length, int M) {
@@ -29,5 +31,26 @@ public class MakeTteok {
             if(result >= M) return result;
         }
         return -1;
+    }
+
+    // 이진 탐색 이용
+    public static int binary(int[] N, int M) {
+        int result = 0;
+        Arrays.sort(N);
+        int start = 0;
+        int end = N[N.length - 1];
+        while(start <= end) {
+            int total = 0;
+            int mid = (start + end) / 2;
+            for(int i: N) {
+                if(i > mid) total += i - mid;
+            }
+            if(total < M) end = mid - 1;
+            else {
+                result = mid;
+                start = mid + 1;
+            }
+        }
+        return result;
     }
 }
